012a64821f51067ae86e879ab7d467e8dada9ee4
Daesiker <eotlr680@naver.com>
2022-09-06 10:33:56 +0900

    init project

---
+++ Melly/Melly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Melly
+//
+//  Created by Jun on 2022/09/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
